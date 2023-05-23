c32c9230e46a5e1d9f218aa0110cf4c6f403d5a1
IvanOrsh <ksironov@mail.ru>
2023-05-23 14:37:32 +0300

    add interface for table of contents

--- interfaces/Markdown.ts
+++ interfaces/Markdown.ts
@@ -1,7 +1,10 @@
+import { TableOfContentsItem } from "./TableOfContentsItem";
+
 export interface Markdown {
   title: string;
   description: string;
   content: string;
   slug: string;
   date: string;
+  tableOfContents?: TableOfContentsItem[];
 }
---
+++ interfaces/TableOfContentsItem.ts
@@ -0,0 +1,5 @@
+export interface TableOfContentsItem {
+  title: string;
+  anchor: string;
+  level: number;
+}

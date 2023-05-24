02b44a50a5c2a5597499188b7ec66d9becc7a822
IvanOrsh <ksironov@mail.ru>
2023-05-24 16:26:52 +0300

    add button

--- components/common/index.tsx
+++ components/common/index.tsx
@@ -2,3 +2,4 @@ export { default as Footer } from "./footer";
 export { default as Header } from "./header";
 export { default as Navbar } from "./navbar";
 export { default as SideMenu } from "./sideMenu";
+export { default as Button } from "./button";
